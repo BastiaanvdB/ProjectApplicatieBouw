8d2f79990521db0ff3a066adb1d25dd9bc93bd8d
Jelleinholland <668419@student.inholland.nl>
2021-05-31 14:22:52 +0200

    Bezig met onderdeel Jelle
    Bezig met mijn onderdeel maar t werkt nog niet 100%

--- Chapoo/ChapooUI/KeukenBarOverzicht.Designer.cs
+++ Chapoo/ChapooUI/KeukenBarOverzicht.Designer.cs
@@ -31,19 +31,34 @@ namespace ChapooUI
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KeukenBarOverzicht));
             this.MenuBarPanel = new System.Windows.Forms.Panel();
+            this.Btn_KeukenOverzicht = new System.Windows.Forms.Button();
+            this.Btn_BarOverzicht = new System.Windows.Forms.Button();
             this.BtnAfmelden = new System.Windows.Forms.Button();
             this.MenuBarAccountPanel = new System.Windows.Forms.Panel();
             this.UserFunctieLabel = new System.Windows.Forms.Label();
             this.UsernameLabel = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Pnl_Baroverzicht = new System.Windows.Forms.Panel();
+            this.listView_Baroverzicht = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.Order_ID = new System.Windows.Forms.ColumnHeader();
+            this.OrderDetailsQuantity = new System.Windows.Forms.ColumnHeader();
+            this.OrderDetails_Comment = new System.Windows.Forms.ColumnHeader();
+            this.Employee_ID = new System.Windows.Forms.ColumnHeader();
+            this.OrderDetails_Ordered_DateTime = new System.Windows.Forms.ColumnHeader();
+            this.OrderDetails_Preparing_DateTime = new System.Windows.Forms.ColumnHeader();
+            this.OrderDetails_Finished_DateTime = new System.Windows.Forms.ColumnHeader();
             this.MenuBarPanel.SuspendLayout();
             this.MenuBarAccountPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Pnl_Baroverzicht.SuspendLayout();
             this.SuspendLayout();
             // 
             // MenuBarPanel
             // 
             this.MenuBarPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.MenuBarPanel.Controls.Add(this.Btn_KeukenOverzicht);
+            this.MenuBarPanel.Controls.Add(this.Btn_BarOverzicht);
             this.MenuBarPanel.Controls.Add(this.BtnAfmelden);
             this.MenuBarPanel.Controls.Add(this.MenuBarAccountPanel);
             this.MenuBarPanel.Dock = System.Windows.Forms.DockStyle.Left;
@@ -51,6 +66,36 @@ namespace ChapooUI
             this.MenuBarPanel.Name = "MenuBarPanel";
             this.MenuBarPanel.Size = new System.Drawing.Size(190, 699);
             this.MenuBarPanel.TabIndex = 0;
+            // 
+            // Btn_KeukenOverzicht
+            // 
+            this.Btn_KeukenOverzicht.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.Btn_KeukenOverzicht.FlatAppearance.BorderSize = 0;
+            this.Btn_KeukenOverzicht.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_KeukenOverzicht.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Btn_KeukenOverzicht.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.Btn_KeukenOverzicht.Location = new System.Drawing.Point(0, 256);
+            this.Btn_KeukenOverzicht.Name = "Btn_KeukenOverzicht";
+            this.Btn_KeukenOverzicht.Size = new System.Drawing.Size(190, 50);
+            this.Btn_KeukenOverzicht.TabIndex = 9;
+            this.Btn_KeukenOverzicht.Text = "Keukenoverzicht";
+            this.Btn_KeukenOverzicht.UseVisualStyleBackColor = false;
+            this.Btn_KeukenOverzicht.Click += new System.EventHandler(this.Btn_KeukenOverzicht_Click_1);
+            // 
+            // Btn_BarOverzicht
+            // 
+            this.Btn_BarOverzicht.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.Btn_BarOverzicht.FlatAppearance.BorderSize = 0;
+            this.Btn_BarOverzicht.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_BarOverzicht.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Btn_BarOverzicht.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.Btn_BarOverzicht.Location = new System.Drawing.Point(0, 200);
+            this.Btn_BarOverzicht.Name = "Btn_BarOverzicht";
+            this.Btn_BarOverzicht.Size = new System.Drawing.Size(190, 50);
+            this.Btn_BarOverzicht.TabIndex = 8;
+            this.Btn_BarOverzicht.Text = "Baroverzicht";
+            this.Btn_BarOverzicht.UseVisualStyleBackColor = false;
+            this.Btn_BarOverzicht.Click += new System.EventHandler(this.Btn_BarOverzicht_Click_1);
             // 
             // BtnAfmelden
             // 
@@ -113,21 +158,98 @@ namespace ChapooUI
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // BarOverzicht
+            // Pnl_Baroverzicht
+            // 
+            this.Pnl_Baroverzicht.Controls.Add(this.listView_Baroverzicht);
+            this.Pnl_Baroverzicht.Location = new System.Drawing.Point(242, 26);
+            this.Pnl_Baroverzicht.Name = "Pnl_Baroverzicht";
+            this.Pnl_Baroverzicht.Size = new System.Drawing.Size(1094, 552);
+            this.Pnl_Baroverzicht.TabIndex = 1;
+            // 
+            // listView_Baroverzicht
+            // 
+            this.listView_Baroverzicht.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.Order_ID,
+            this.OrderDetailsQuantity,
+            this.OrderDetails_Comment,
+            this.Employee_ID,
+            this.OrderDetails_Ordered_DateTime,
+            this.OrderDetails_Preparing_DateTime,
+            this.OrderDetails_Finished_DateTime});
+            this.listView_Baroverzicht.HideSelection = false;
+            this.listView_Baroverzicht.Location = new System.Drawing.Point(25, 15);
+            this.listView_Baroverzicht.Name = "listView_Baroverzicht";
+            this.listView_Baroverzicht.Size = new System.Drawing.Size(1047, 517);
+            this.listView_Baroverzicht.TabIndex = 0;
+            this.listView_Baroverzicht.UseCompatibleStateImageBehavior = false;
+            this.listView_Baroverzicht.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Name = "columnHeader1";
+            this.columnHeader1.Text = "Item_ID";
+            this.columnHeader1.Width = 100;
+            // 
+            // Order_ID
+            // 
+            this.Order_ID.Name = "Order_ID";
+            this.Order_ID.Text = "Order_ID";
+            this.Order_ID.Width = 100;
+            // 
+            // OrderDetailsQuantity
+            // 
+            this.OrderDetailsQuantity.Name = "OrderDetailsQuantity";
+            this.OrderDetailsQuantity.Text = "Quantity";
+            this.OrderDetailsQuantity.Width = 100;
+            // 
+            // OrderDetails_Comment
+            // 
+            this.OrderDetails_Comment.Name = "OrderDetails_Comment";
+            this.OrderDetails_Comment.Text = "Comment";
+            this.OrderDetails_Comment.Width = 200;
+            // 
+            // Employee_ID
+            // 
+            this.Employee_ID.Name = "Employee_ID";
+            this.Employee_ID.Text = "Employee_ID";
+            this.Employee_ID.Width = 100;
+            // 
+            // OrderDetails_Ordered_DateTime
+            // 
+            this.OrderDetails_Ordered_DateTime.Name = "OrderDetails_Ordered_DateTime";
+            this.OrderDetails_Ordered_DateTime.Text = "Time orderd";
+            this.OrderDetails_Ordered_DateTime.Width = 150;
+            // 
+            // OrderDetails_Preparing_DateTime
+            // 
+            this.OrderDetails_Preparing_DateTime.Name = "OrderDetails_Preparing_DateTime";
+            this.OrderDetails_Preparing_DateTime.Text = "Time of preparing";
+            this.OrderDetails_Preparing_DateTime.Width = 150;
+            // 
+            // OrderDetails_Finished_DateTime
+            // 
+            this.OrderDetails_Finished_DateTime.Name = "OrderDetails_Finished_DateTime";
+            this.OrderDetails_Finished_DateTime.Text = "Time Finished";
+            this.OrderDetails_Finished_DateTime.Width = 150;
+            // 
+            // KeukenBarOverzicht
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
             this.ClientSize = new System.Drawing.Size(1399, 699);
+            this.Controls.Add(this.Pnl_Baroverzicht);
             this.Controls.Add(this.MenuBarPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "BarOverzicht";
+            this.Name = "KeukenBarOverzicht";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Chapoo Bar overzicht";
             this.MenuBarPanel.ResumeLayout(false);
             this.MenuBarAccountPanel.ResumeLayout(false);
             this.MenuBarAccountPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Pnl_Baroverzicht.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -140,6 +262,19 @@ namespace ChapooUI
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label UserFunctieLabel;
         private System.Windows.Forms.Button BtnAfmelden;
+        private System.Windows.Forms.Panel Pnl_Baroverzicht;
+        private System.Windows.Forms.ListView listView_Baroverzicht;
+        private System.Windows.Forms.Button Btn_KeukenOverzicht;
+        private System.Windows.Forms.Button Btn_BarOverzicht;
+        private System.Windows.Forms.ColumnHeader ItemID;
+        private System.Windows.Forms.ColumnHeader Order_ID;
+        private System.Windows.Forms.ColumnHeader OrderDetailsQuantity;
+        private System.Windows.Forms.ColumnHeader OrderDetails_Comment;
+        private System.Windows.Forms.ColumnHeader Employee_ID;
+        private System.Windows.Forms.ColumnHeader OrderDetails_Ordered_DateTime;
+        private System.Windows.Forms.ColumnHeader OrderDetails_Preparing_DateTime;
+        private System.Windows.Forms.ColumnHeader OrderDetails_Finished_DateTime;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
     }
 }
 
